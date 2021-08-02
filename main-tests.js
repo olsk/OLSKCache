74e505060834d797bc064901bf4e44ed89d1ed77
Rosano Coutinho <pub@xeari.com>
2021-08-02 20:17:50 -0300

    Spec OLSKCacheQueuedFetch

--- main-tests.js
+++ main-tests.js
@@ -529,3 +529,399 @@ describe('OLSKCachePath', function test_OLSKCachePath() {
 	});
 
 });
+
+describe('OLSKCacheQueuedFetch', function test_OLSKCacheQueuedFetch() {
+
+	const _OLSKCacheQueuedFetch = function (inputData) {
+		return mod.OLSKCacheQueuedFetch(Object.assign({
+			ParamMod: {},
+			ParamKey: Math.random().toString(),
+			ParamCallback: (function () {}),
+			ParamInterval: 1,
+
+			_ParamCallbackDidFinish: (function () {
+				clearInterval(this.ParamMod._OLSKCacheTimerMap[this.ParamKey]);
+			}),
+
+			OLSKQueue: Object.assign({
+				OLSKQueueAPI: (function () {}),
+			}, inputData),
+
+			OLSKDisk: Object.assign({
+				OLSKDiskWrite: (function () {}),
+			}, inputData),
+			ParamFileDirectory: Math.random().toString(),
+		}, inputData));
+	};
+
+	it('rejects if not object', function() {
+		return rejects(mod.OLSKCacheQueuedFetch(null), /OLSKErrorInputNotValid/);
+	});
+
+	it('rejects if ParamMod not object', function() {
+		return rejects(_OLSKCacheQueuedFetch({
+			ParamMod: null,
+		}), /OLSKErrorInputNotValid/);
+	});
+	
+	it('rejects if ParamKey not string', function() {
+		return rejects(_OLSKCacheQueuedFetch({
+			ParamKey: null,
+		}), /OLSKErrorInputNotValid/);
+	});
+	
+	it('rejects if ParamCallback not function', function() {
+		return rejects(_OLSKCacheQueuedFetch({
+			ParamCallback: null,
+		}), /OLSKErrorInputNotValid/);
+	});
+	
+	it('rejects if ParamInterval not number', function() {
+		return rejects(_OLSKCacheQueuedFetch({
+			ParamInterval: '1',
+		}), /OLSKErrorInputNotValid/);
+	});
+
+	it('rejects if OLSKQueue not object', function() {
+		return rejects(_OLSKCacheQueuedFetch({
+			OLSKQueue: null,
+		}), /OLSKErrorInputNotValid/);
+	});
+
+	context('ParamFileURLs', function () {
+		
+		it('rejects if not array', function() {
+			return rejects(_OLSKCacheQueuedFetch({
+				ParamFileURLs: null,
+			}), /OLSKErrorInputNotValid/);
+		});
+
+		it('rejects if OLSKDisk not array', function() {
+			return rejects(_OLSKCacheQueuedFetch({
+				ParamFileURLs: [],
+				OLSKDisk: null,
+			}), /OLSKErrorInputNotValid/);
+		});
+
+		it('rejects if ParamFileDirectory not string', function() {
+			return rejects(_OLSKCacheQueuedFetch({
+				ParamFileURLs: [],
+				ParamFileDirectory: null,
+			}), /OLSKErrorInputNotValid/);
+		});
+
+		it('rejects if ParamFileJSON not boolean', function() {
+			return rejects(_OLSKCacheQueuedFetch({
+				ParamFileURLs: [],
+				ParamFileJSON: 'true',
+			}), /OLSKErrorInputNotValid/);
+		});
+	
+	});
+
+	context('not_filled', function () {
+
+		it('returns ParamCallback result', async function() {
+			const item = Math.random().toString();
+
+			deepEqual(await _OLSKCacheQueuedFetch({
+				ParamCallback: (function () {
+					return item;
+				}),
+			}), item);
+		});
+
+		it('assigns result to _OLSKCacheResultMap[key]', async function () {
+			const _OLSKCacheResultMap = {};
+
+			const ParamKey = Math.random().toString();
+
+			await _OLSKCacheQueuedFetch({
+				ParamMod: {
+					_OLSKCacheResultMap,
+				},
+				ParamKey,
+				ParamCallback: (function () {
+					return ParamKey;
+				}),
+			});
+
+			deepEqual(_OLSKCacheResultMap, {
+				[ParamKey]: ParamKey,
+			});
+		});
+
+	});
+
+	context('filled', function () {
+		
+		it('returns _OLSKCacheResultMap[key]', async function() {
+			const ParamKey = Math.random().toString();
+
+			deepEqual(await _OLSKCacheQueuedFetch({
+				ParamMod: {
+					_OLSKCacheResultMap: {
+						[ParamKey]: ParamKey,
+					},
+				},
+				ParamKey,
+			}), ParamKey);
+		});
+
+		it('skips ParamCallback', async function() {
+			const ParamKey = Math.random().toString();
+
+			deepEqual(await _OLSKCacheQueuedFetch({
+				ParamMod: {
+					_OLSKCacheResultMap: {
+						[ParamKey]: ParamKey,
+					},
+				},
+				ParamKey,
+				ParamCallback: (function () {
+					return Math.random().toString();
+				}),
+			}), ParamKey);
+		});
+	
+	});
+
+	context('ParamCallback', function () {
+		
+		it('repeats at ParamInterval', async function() {
+			const ParamKey = Math.random().toString();
+			const _OLSKCacheResultMap = {
+				[ParamKey]: 1,
+			};
+			const item = Math.max(3, uRandomInt(4));
+
+			await _OLSKCacheQueuedFetch({
+				ParamMod: {
+					_OLSKCacheResultMap,
+				},
+				ParamKey,
+				ParamCallback: (function () {
+					return _OLSKCacheResultMap[ParamKey] + 1;
+				}),
+				_ParamCallbackDidFinish: (function () {
+					if (_OLSKCacheResultMap[ParamKey] >= item) {
+						clearInterval(this.ParamMod._OLSKCacheTimerMap[this.ParamKey]);
+					}
+				}),
+			});
+
+			await (new Promise(function (res, rej) {
+				return setTimeout(res, 5 * item);
+			}));
+			
+			deepEqual(_OLSKCacheResultMap, {
+				[ParamKey]: item,
+			});
+		});
+
+		context('ParamFileURLs', function () {
+			
+			it('calls OLSKDiskWrite', async function () {
+				const ParamFileDirectory = Math.random().toString();
+				const ParamKey = uLink();
+				const item = Math.random().toString();
+				
+				deepEqual(await uCapture(async function (OLSKDiskWrite) {
+					await _OLSKCacheQueuedFetch({
+						ParamKey,
+						ParamCallback: (function () {
+							return item;
+						}),
+						ParamFileURLs: [],
+						OLSKDiskWrite,
+						ParamFileDirectory,
+					});
+				}), [mod.OLSKCachePath(ParamFileDirectory, mod.OLSKCacheURLBasename(ParamKey)), item]);
+			});
+
+			context('ParamFileJSON', function () {
+
+				it('calls JSON.stringify before write', async function () {
+					const ParamFileDirectory = Math.random().toString();
+					const ParamKey = uLink();
+					const item = {
+						[Math.random().toString()]: Math.random().toString(),
+					};
+					
+					deepEqual(await uCapture(async function (OLSKDiskWrite) {
+						await _OLSKCacheQueuedFetch({
+							ParamKey,
+							ParamCallback: (function () {
+								return item;
+							}),
+							ParamFileURLs: [],
+							ParamFileJSON: true,
+							OLSKDiskWrite,
+							ParamFileDirectory,
+						});
+					}), [mod.OLSKCachePath(ParamFileDirectory, mod.OLSKCacheURLBasename(ParamKey)), JSON.stringify(item)]);
+				});
+			
+			});
+		
+		});
+	
+	});
+
+	context('SetupQueue', function test_SetupQueue() {
+
+		it('calls OLSKQueueAPI', function () {
+			const item = Math.random().toString();
+			deepEqual(uCapture(function (capture) {
+				_OLSKCacheQueuedFetch({
+					OLSKQueueAPI: (function () {
+						capture(item);
+					}),
+				})
+			}), [item]);
+		});
+
+		it('sets ParamMod[_OLSKCacheQueue] if not filled', function () {
+			const item = Math.random().toString();
+			const ParamMod = {};
+
+			_OLSKCacheQueuedFetch({
+				ParamMod,
+				OLSKQueueAPI: (function () {
+					return item;
+				}),
+			})
+			deepEqual(ParamMod._OLSKCacheQueue, item);
+		});
+
+		it('skips set ParamMod[_OLSKCacheQueue] if filled', function () {
+			const _OLSKCacheQueue = Math.random().toString();
+			const ParamMod = {
+				_OLSKCacheQueue,
+			};
+
+			_OLSKCacheQueuedFetch({
+				ParamMod,
+				OLSKQueueAPI: (function () {
+					return Math.random().toString();
+				}),
+			})
+			deepEqual(ParamMod._OLSKCacheQueue, _OLSKCacheQueue);
+		});
+
+	});
+
+	context('SetupResultMap', function test_SetupResultMap() {
+
+		it('sets ParamMod[_OLSKCacheResultMap] if not filled', function () {
+			const ParamMod = {};
+
+			_OLSKCacheQueuedFetch({
+				ParamMod,
+			})
+			deepEqual(ParamMod._OLSKCacheResultMap, {});
+		});
+
+		it('skips set ParamMod[_OLSKCacheResultMap] if filled', function () {
+			const _OLSKCacheResultMap = Math.random().toString();
+			const ParamMod = {
+				_OLSKCacheResultMap,
+			};
+
+			_OLSKCacheQueuedFetch({
+				ParamMod,
+			})
+			deepEqual(ParamMod._OLSKCacheResultMap, _OLSKCacheResultMap);
+		});
+
+		context('ParamFileURLs', function () {
+			
+			it('calls OLSKDiskRead', function () {
+				const ParamFileDirectory = Math.random().toString();
+				const url = uLink();
+				
+				deepEqual(uCapture(function (OLSKDiskRead) {
+					_OLSKCacheQueuedFetch({
+						ParamFileURLs: [url],
+						OLSKDiskRead,
+						ParamFileDirectory
+					});
+				}), [mod.OLSKCachePath(ParamFileDirectory, mod.OLSKCacheURLBasename(url))]);
+			});
+
+			it('sets ParamMod[_OLSKCacheResultMap] if OLSKDiskRead filled', async function () {
+				const ParamMod = {};
+				const ParamKey = uLink();
+				const OLSKDiskRead = uRandomElement(Math.random().toString(), null);
+
+				await _OLSKCacheQueuedFetch({
+					ParamMod,
+					ParamKey,
+					ParamFileURLs: [ParamKey],
+					OLSKDiskRead: (function () {
+						return OLSKDiskRead;
+					}),
+				});
+
+				deepEqual(ParamMod._OLSKCacheResultMap, {
+					[ParamKey]: OLSKDiskRead,
+				});
+			});
+
+			context('ParamFileJSON', function () {
+
+				it('calls JSON.parse after read', async function () {
+					const ParamMod = {};
+					const ParamKey = uLink();
+					const OLSKDiskRead = {
+						[Math.random().toString()]: Math.random().toString(),
+					};
+
+					await _OLSKCacheQueuedFetch({
+						ParamMod,
+						ParamKey,
+						ParamFileURLs: [ParamKey],
+						ParamFileJSON: true,
+						OLSKDiskRead: (function () {
+							return JSON.stringify(OLSKDiskRead);
+						}),
+					});
+
+					deepEqual(ParamMod._OLSKCacheResultMap[ParamKey], OLSKDiskRead);
+				});
+			
+			});
+		
+		});
+
+	});
+
+	context('SetupTimerMap', function test_SetupTimerMap() {
+
+		it('sets ParamMod[_OLSKCacheTimerMap] if not filled', function () {
+			const ParamMod = {};
+
+			_OLSKCacheQueuedFetch({
+				ParamMod,
+			})
+			deepEqual(ParamMod._OLSKCacheTimerMap, {});
+		});
+
+		it('skips set ParamMod[_OLSKCacheTimerMap] if filled', function () {
+			const _OLSKCacheTimerMap = {
+				[Math.random().toString()]: Math.random().toString(),
+			};
+			const ParamMod = {
+				_OLSKCacheTimerMap: Object.assign({}, _OLSKCacheTimerMap),
+			};
+
+			_OLSKCacheQueuedFetch({
+				ParamMod,
+			});
+
+			deepEqual(ParamMod._OLSKCacheTimerMap, _OLSKCacheTimerMap);
+		});
+
+	});
+
+});
--- main.js
+++ main.js
@@ -170,6 +170,88 @@ const mod = {
 		return _require('path').join(...[inputData, '__cached'].concat([...arguments].slice(1)));
 	},
 
+	async OLSKCacheQueuedFetch (params) {
+		if (typeof params !== 'object' || params === null) {
+			return Promise.reject('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamMod !== 'object' || params.ParamMod === null) {
+			return Promise.reject('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamKey !== 'string') {
+			return Promise.reject('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamCallback !== 'function') {
+			return Promise.reject('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamInterval !== 'number') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.OLSKQueue !== 'object' || params.OLSKQueue === null) {
+			return Promise.reject('OLSKErrorInputNotValid');
+		}
+
+		if (params.ParamFileURLs !== undefined) {
+			if (!Array.isArray(params.ParamFileURLs)) {
+				return Promise.reject('OLSKErrorInputNotValid');
+			}
+
+			if (typeof params.OLSKDisk !== 'object' || params.OLSKDisk === null) {
+				return Promise.reject('OLSKErrorInputNotValid');
+			}
+
+			if (typeof params.ParamFileDirectory !== 'string') {
+				return Promise.reject('OLSKErrorInputNotValid');
+			}
+
+			if (params.ParamFileJSON && typeof params.ParamFileJSON !== 'boolean') {
+				return Promise.reject('OLSKErrorInputNotValid');
+			}
+		}
+
+		(function SetupQueue () {
+			params.ParamMod._OLSKCacheQueue = params.ParamMod._OLSKCacheQueue || params.OLSKQueue.OLSKQueueAPI();
+		})();
+
+		(function SetupResultMap () {
+			params.ParamMod._OLSKCacheResultMap = params.ParamMod._OLSKCacheResultMap || (params.ParamFileURLs || []).reduce(function (coll, item) {
+				return Object.assign(coll, {
+					[item]: (function(inputData) {
+						return params.ParamFileJSON ? JSON.parse(inputData) : inputData;
+					})(params.OLSKDisk.OLSKDiskRead(mod.OLSKCachePath(params.ParamFileDirectory, mod.OLSKCacheURLBasename(item)))),
+				});
+			}, {});
+		})();
+
+		(function SetupTimerMap () {
+			params.ParamMod._OLSKCacheTimerMap = params.ParamMod._OLSKCacheTimerMap || {};
+		})();
+
+		const callback = async function () {
+			params.ParamMod._OLSKCacheResultMap[params.ParamKey] = await params.ParamCallback();
+
+			if (params.ParamFileURLs) {
+				params.OLSKDisk.OLSKDiskWrite(mod.OLSKCachePath(params.ParamFileDirectory, mod.OLSKCacheURLBasename(params.ParamKey)), (function(inputData) {
+						return params.ParamFileJSON ? JSON.stringify(inputData) : inputData;
+					})(params.ParamMod._OLSKCacheResultMap[params.ParamKey]));
+			}
+
+			return params._ParamCallbackDidFinish && params._ParamCallbackDidFinish();
+		};
+
+		if (!params.ParamMod._OLSKCacheResultMap[params.ParamKey]) {
+			await callback();
+		}
+
+		params.ParamMod._OLSKCacheTimerMap[params.ParamKey] = params.ParamMod._OLSKCacheTimerMap[params.ParamKey] || setInterval(callback, params.ParamInterval);
+
+		return params.ParamMod._OLSKCacheResultMap[params.ParamKey];
+	},
+
 };
 
 Object.assign(exports, mod);
